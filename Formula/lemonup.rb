dd2201b7b59a724f8980830905e1116c942632c0
Daniel Shneyder <archcorsair@gmail.com>
2025-12-18 18:35:51 -0800

    fix: add missing sha256 hashes

--- Formula/lemonup.rb
+++ Formula/lemonup.rb
@@ -6,13 +6,13 @@ class Lemonup < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/archcorsair/lemonup/releases/download/v0.0.2/lemonup-macos-arm64"
-    sha256 "REPLACE_WITH_MACOS_ARM64_SHA"
+    sha256 "69e261baa62272251e87c1fdc60958b35820c8a5387145412286005f3ae7dc76"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/archcorsair/lemonup/releases/download/v0.0.2/lemonup-macos-x64"
-    sha256 "REPLACE_WITH_MACOS_X64_SHA"
+    sha256 "024821b737c477144fa4ad460c2f6c4af10ca3278e22ad71f37846aa32dd586e"
   elsif OS.linux?
     url "https://github.com/archcorsair/lemonup/releases/download/v0.0.2/lemonup-linux-x64"
-    sha256 "REPLACE_WITH_LINUX_SHA"
+    sha256 "c70315957a5bbf1ae4daec3e580600e964430665bac205c615d035be6228f9a5"
   end
 
   def install
